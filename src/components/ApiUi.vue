87e323bc309f0b487ca80dd32ee320a44d3cf1c8
Toby Twigger <tt15951@my.bristol.ac.uk>
2020-01-25 01:33:31 +0000

    Initial Commit

---
+++ src/components/ApiUi.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div ref="api-container"></div>
+    </div>
+</template>
+
+<script>
+    import SwaggerUI, {presets} from 'swagger-ui';
+    import 'swagger-ui/dist/swagger-ui.css';
+
+    export default {
+        name: 'ApiUI',
+        props: {
+            username: {
+                required: true,
+                type: String
+            },
+            repository: {
+                required: true,
+                type: String
+            },
+            branch: {
+                required: true,
+                type: String
+            }
+        },
+        data() {
+            return {
+            }
+        },
+        mounted() {
+            SwaggerUI({
+                domNode: this.$refs['api-container'],
+                url: this.swaggerUrl,
+                presets: [presets.apis],
+            });
+        },
+        computed: {
+            swaggerUrl() {
+                return 'https://github.com/'
+                    + this.username + '/'
+                    + this.repository
+                    + '/blob/'
+                    + this.branch
+                    + '/swagger.json';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LatestVersion.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <api-ui
+            username="bristol-su"
+            repository="control-api-docs"
+            branch="latest"></api-ui>
+    </div>
+</template>
+
+<script>
+    import ApiUi from "../components/ApiUi";
+
+    export default {
+        name: 'LatestVersion',
+        components: {
+            ApiUi
+        },
+        data() {
+            return {
+            }
+        }
+    }
+</script>
